5ea16bab12a52841b4b53b84ca68f61fc600f129
Garden0512 <12gundam05@gmail.com>
2025-09-24 12:47:32 +0900

    이항계수 문제 해결

--- src/bin/problem11050.rs
+++ src/bin/problem11050.rs
@@ -1,4 +1,39 @@
+use std::io::{self, BufRead};
+
 fn main()
 {
-    println!("Hello, world!");
+    let mut input = String::new();
+    let mut reader = io::stdin().lock();
+    reader
+        .read_line(&mut input)
+        .expect("Error");
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let a:i32 = input.next().expect("Error");
+    let b:i32 = input.next().expect("Error");
+    let n:i32;
+    let k:i32;
+    if a<b
+    {
+        n = b;
+        k = a;
+    }
+    else
+    {
+        n = a;
+        k = b;
+    }
+    let nck = factorial(n)/(factorial(n-k) * factorial(k));
+    println!("{}", nck);
+}
+
+fn factorial(n:i32) -> i32
+{
+    if n==0||n==1
+    {
+        return 1;
+    }
+    else
+    {
+        return n*factorial(n-1);
+    }
 }
